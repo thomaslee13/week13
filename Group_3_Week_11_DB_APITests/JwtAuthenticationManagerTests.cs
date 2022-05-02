b8b27f0d6644a2a771d2106ca62c147e65e2e7c3
thomaslee13 <ev5130ke@go.minnstate.edu>
2022-05-01 22:04:54 -0500

    Unit tests added.

---
+++ Group_3_Week_11_DB_APITests/JwtAuthenticationManagerTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Group_3_Week_11_DB_API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group_3_Week_11_DB_API.Tests
+{
+    [TestClass()]
+    public class JwtAuthenticationManagerTests
+    {
+        [TestMethod()]
+        public void AuthenticateTestFail()
+        {
+            JwtAuthenticationManager manager = new JwtAuthenticationManager("fakekeynotlegit1111!");
+
+            User user = new User
+            {
+                username = "testuser",
+                password = "testpassword"
+            };
+
+            var ret = manager.Authenticate(user.username, user.password);
+
+            Assert.IsNull(ret);
+        }
+
+
+        [TestMethod()]
+        public void AuthenticateTestPass()
+        {
+            JwtAuthenticationManager manager = new JwtAuthenticationManager("fakekeynotlegit1111!");
+
+            User user = new User
+            {
+                username = "test",
+                password = "password"
+            };
+
+            var ret = manager.Authenticate(user.username, user.password);
+
+            Assert.IsNotNull(ret);
+        }
+    }
+}
